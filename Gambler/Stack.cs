493c09544d528c2f72fd0221034fe6cd775f5af2
Aftab-1998 <alam41976@gmail.com>
2021-12-23 10:10:41 +0530

    [Aftab] Add ; Added the game started

---
+++ Gambler/Stack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gambler
+{
+        public class Stack
+        {
+            public void StartGame()
+            {
+                int stackmoney = 100;
+                int betmoney = 1;
+                Console.WriteLine($"the money is : {stackmoney}");
+                 Console.WriteLine($"the money is : {betmoney}");
+            }
+        }
+}
